d78476b7458f56f076081a3a635bff559451e7d7
lj11 <lj11@strongsoft.net>
2018-04-27 02:03:46 +0800

    First commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,16 @@
 <template>
     <div class="container">
         <h3 class="title">系统登录</h3>
-
-        <Input  :text="'账号:'" :inputStyle="inputStyle1" :name="$api.login.p.uname" v-model="login_uname" @on-input="onInput"></Input>
-        <Input  :text="'密码:'" :inputStyle="inputStyle1" :name="$api.login.p.pass" v-model="login_pass"></Input>
-        <Input  :inputStyle="inputStyle2" :text="'验证码:'" :name="$api.login.p.code">
+        <label>账号:</label><i-input v-model="login_uname"></i-input>
+        <label>密码:</label><i-input v-model="login_pass"></i-input>
+        <label>验证码:</label><i-input v-model="login_code"></i-input>
         <img src="http://localhost:8080/pallas/sys.do?fn=getcode&p=%7B%7D" width="70" height="35" @click="changeImg" />
-        </Input>
-        <i-input v-model="login_uname" style="margin-top: 100px"></i-input>
-        <i-input v-model="login_pass"></i-input>
-        <i-input v-model="login_code"></i-input>
-        <Button @on-click="$get($api.login,{vm:self})">登录</Button>
+        <Button type="info" @click="$get($api.login,{vm:self})">登录</Button>
     </div>
 </template>
 
 <script>
     import Input from '@/views/components/Input.vue'
-    import Button from '@/views/components/Button.vue'
     import {initSkin} from "../lib/skin.js"
     export default {
         name: 'buttons',
@@ -41,7 +35,7 @@
             //this.$apiFn.gets(this.$api.login,{vm:this})
     },
         components: {
-           Input,Button
+           Input
         },
         mounted(){
 
@@ -80,11 +74,12 @@
         width: pxTorem(500px);
         padding: 0.46rem 0.46rem 0.26rem 0.46rem;
         background: #fff;
+        display: inline-block;
     }
     .container img{
-        position: absolute;
-        top: 0.13rem;
-        right: 0.6rem;
+        /*position: absolute;*/
+        /*top: 0.13rem;*/
+        /*right: 0.6rem;*/
     }
     .container h3{
         margin: 0px auto 0.5rem auto;
